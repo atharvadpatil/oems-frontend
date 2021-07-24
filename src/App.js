8cefaab900558db3e7b11e644830c13020f7044e
Viraj Patidar <vrj381@gmail.com>
2021-07-24 11:02:38 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <Switch>
+        <Route path = "/" component = {Login} exact />
+        <Route component = {NotFound} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+
+const NotFound = () => {
+  return (
+    <h1>404 Not Found</h1>
+  );
+}
