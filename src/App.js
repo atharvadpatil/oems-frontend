a9b55b53a89d52c8e864d0e1cf731cd1aeef4e3a
Viraj Patidar <vrj381@gmail.com>
2021-07-26 15:46:52 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Login from './components/login';
 import Register from './components/register';
 import NotFound from './components/NotFound';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
+      <Navbar />
       <Switch>
         <Route path = "/" component = {Login} exact />
         <Route path = "/register" component = {Register} exact />
